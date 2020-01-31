e317533252eda2c8650ea25b4b62b66a2b1fa247
Gabriela Voll <gabriela.voll@youvisit.com>
2020-01-31 01:32:03 -0500

    Updates

--- __test__/index.test.jsx
+++
@@ -1,5 +0,0 @@
-describe("Example Test", () => {
-	test("should run test", () => {
-		expect(true).toBeTruthy();
-	});
-});
---
+++ precache-manifest.17a286c486a0967e1185859ee1aef92e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e1889e300d92a7853d81a81b8563dd",
+    "url": "/okc_web_takehome/index.html"
+  },
+  {
+    "revision": "90350ffadcc426711b50",
+    "url": "/okc_web_takehome/static/css/main.871a2aba.chunk.css"
+  },
+  {
+    "revision": "4c3e411d974b5a7aaa67",
+    "url": "/okc_web_takehome/static/js/2.3ac422dd.chunk.js"
+  },
+  {
+    "revision": "90350ffadcc426711b50",
+    "url": "/okc_web_takehome/static/js/main.a7525616.chunk.js"
+  },
+  {
+    "revision": "bd623dda0d24943424a7",
+    "url": "/okc_web_takehome/static/js/runtime~main.ebdc0c0f.js"
+  }
+]);
